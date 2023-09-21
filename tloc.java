e17b515baad9ea8166f97bb4f9885c21170dfb56
emilybazilesupplice <105016329+emilybazilesupplice@users.noreply.github.com>
2023-09-21 17:45:05 -0400

    Exercice 1 : commentaire bloc
    Prend en compte les commentaires en bloc

--- tloc.java
+++ tloc.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class tloc {
+    /* Cette fonction va compter le nombre de ligne qui ne sont ni des lignes vides, ni des commentaires */
 
     public static void main(String[] args) throws IOException {
         // aller chercher le fichier à lire
@@ -11,23 +12,39 @@ public class tloc {
         
         String ligne;
         int n = 0;
+        boolean commentBlock = false;
         while((ligne = read.readLine()) != null) {
             ligne = ligne.trim();
 
-            // Vérifier que la ligne n'est pas vide
+            // Vérifie si la ligne est vice
             if (ligne.isEmpty()){
                 continue;
             }
             
-            // Vérifie qu'il n'y a pas de commentaire
+            // Vérifie si la ligne est un commentaire
             if (ligne.startsWith("//")){
+                continue;
+            }
+
+            // Vérifie si la ligne est un bloc de commentaire
+            if (commentBlock) {
+                if (ligne.contains("*/")) {
+                    commentBlock = false;
+                }
+                continue;
+            }
+            if (ligne.startsWith("/*")) {
+                if (ligne.contains("*/")){
+                    continue;
+                }
+                commentBlock = true;
                 continue;
             }
 
             // On augmente le compte
             n = n + 1;
         }
-        
+
         read.close();
 
     }
